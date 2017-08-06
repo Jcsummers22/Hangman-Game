5df9ce3233f505cb4c0221c05955b6c2a986a2c1
Jason <jcsummers04@gmail.com>
2017-08-05 20:36:36 -0700

    temporary work

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+
+	var alaphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n"
+	"o", "p", "q", "r", "s", "t","u", "v", "w", "x", "y", "z"];
+	var guessedLetters = [];
+	var words = ["mario", "zelda", "supersmashbros", "starcraft", "streetfigher", "diablo", "overwatch"];
+	var wins = 0;
+	var losses = 0;
+	var userGuess = event.key;
+	var lives = 9;
